b023037949fc28cf0a6a449fea1533e4adbbd0e2
kazeusagi <toshiki1098@gmail.com>
2025-02-22 14:08:46 +0000

    chore: ESLintとprettierをbiomeに置き換え

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+	const config = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.addTag('cats')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
